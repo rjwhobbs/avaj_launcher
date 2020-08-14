42880956d5bc634f543af36baf37675e0d11ad50
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-08-14 18:59:36 +0200

    Abstract classes can have static methods

--- src/com/rhobbs/simulator/Main.java
+++ src/com/rhobbs/simulator/Main.java
@@ -5,13 +5,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+abstract class Test {
+  public static void method(String s) {
+    System.out.println(s);
+  }
+}
+
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) /* throws InterruptedException */ {
       try {
+        Test.method("MA BRUS");
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
         String line = reader.readLine();
-        System.out.println(line);
+        if (line != null) {
+          String[] strArr = line.split(" ");
+          for (int i = 0; i < strArr.length; i++) {
+            System.out.println(strArr[i]);
+          }
+        }
       } catch (FileNotFoundException e) {
         System.out.println(args[0] + " : File not found");
       } catch (IOException e) {
