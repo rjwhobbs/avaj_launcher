535c767dce41b1bf567f370fa817998b06e3cdd1
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-08-20 17:12:06 +0200

    Fixed fatal flaw in unregitration method

--- src/com/rhobbs/simulator/Tower.java
+++ src/com/rhobbs/simulator/Tower.java
@@ -8,6 +8,7 @@ import java.util.List;
 public abstract class Tower {
 
   private List<Flyable> observers = new ArrayList<>();
+  private static boolean didUnresgiter = false;
 
   public void register(Flyable flyable) {
     observers.add(flyable);
@@ -15,6 +16,7 @@ public abstract class Tower {
 
   public void unregister(Flyable flyable) {
     observers.remove(flyable);
+    didUnresgiter = true;
   }
 
   protected void conditionsChanged() {
@@ -22,10 +24,10 @@ public abstract class Tower {
     while (i < observers.size()) {
       observers.get(i).updateConditions();
       i++;
+      if (didUnresgiter) {
+        didUnresgiter = false;
+        i--;
+      }
     }
-
   }
-
-
-
 }
